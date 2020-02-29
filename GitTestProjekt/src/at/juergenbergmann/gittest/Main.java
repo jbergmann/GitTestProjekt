b443438bf595ca9ee898201119c4ebda6081cd9d
juerg <juerg@Lenovo-Ideapad.lan>
2020-02-29 12:29:45 +0100

    Test

--- GitTestProjekt/src/at/juergenbergmann/gittest/Main.java
+++ GitTestProjekt/src/at/juergenbergmann/gittest/Main.java
@@ -1,10 +1,13 @@
 package at.juergenbergmann.gittest;
 
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Person person = new Person("Jürgen Bergmann", LocalDate.now());
+		System.out.println(person.toString());
 	}
 
 }
--- GitTestProjekt/src/at/juergenbergmann/gittest/Person.java
+++ GitTestProjekt/src/at/juergenbergmann/gittest/Person.java
@@ -1,5 +1,22 @@
 package at.juergenbergmann.gittest;
 
-public class Person {
+import java.time.LocalDate;
 
+public class Person {
+	String name;
+	LocalDate geburtsDatum;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public Person(String name, LocalDate geburtsDatum) {
+		this.name = name;
+		this.geburtsDatum = geburtsDatum;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", geburtsDatum=" + geburtsDatum + "]";
+	}
 }
